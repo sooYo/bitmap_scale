69ab920ff778373185809947da51c4ccfefd3caa
sooyo <xiao.chu.go@163.com>
2021-07-27 18:07:06 +0800

    Return origin bitmap if transform encouters error

--- app/src/main/java/com/quexb/testbitmapscale/image/BitmapBoxFit.kt
+++ app/src/main/java/com/quexb/testbitmapscale/image/BitmapBoxFit.kt
@@ -2,10 +2,16 @@ package com.quexb.testbitmapscale.image
 
 import android.graphics.Bitmap
 import android.graphics.Matrix
+import android.util.Log
 import kotlin.math.roundToInt
 
 fun Bitmap.boxFitFill(width: Int, height: Int): Bitmap {
-    return Bitmap.createScaledBitmap(this, width, height, true)
+    return try {
+        Bitmap.createScaledBitmap(this, width, height, true)
+    } catch (e: Exception) {
+        Log.e("BitmapUtils", "boxFitFill: $e")
+        this
+    }
 }
 
 fun Bitmap.boxFitContains(width: Int, height: Int): Bitmap {
@@ -17,45 +23,61 @@ fun Bitmap.boxFitCover(width: Int, height: Int): Bitmap {
 }
 
 fun Bitmap.boxFitFitWidth(width: Int): Bitmap {
-    val ratio: Float = this.width.toFloat() / this.height.toFloat()
-    val height: Int = (width / ratio).roundToInt()
+    return try {
+        val ratio = this.width.toFloat() / this.height.toFloat()
+        val height = (width / ratio).roundToInt()
 
-    return Bitmap.createScaledBitmap(
-        this,
-        width,
-        height,
-        true
-    )
+        Bitmap.createScaledBitmap(
+            this,
+            width,
+            height,
+            true
+        )
+    } catch (e: Exception) {
+        Log.e("BitmapUtils", "boxFitFitWidth: $e")
+        this
+    }
 }
 
 fun Bitmap.boxFitFitHeight(height: Int): Bitmap {
-    val ratio: Float = this.height.toFloat() / this.width.toFloat()
-    val width: Int = (height / ratio).roundToInt()
+    return try {
+        val ratio = this.height.toFloat() / this.width.toFloat()
+        val width = (height / ratio).roundToInt()
 
-    return Bitmap.createScaledBitmap(
-        this,
-        width,
-        height,
-        true
-    )
+        Bitmap.createScaledBitmap(
+            this,
+            width,
+            height,
+            true
+        )
+    } catch (e: Exception) {
+        Log.e("BitmapUtils", "boxFitFitHeight: $e")
+        this
+    }
 }
 
 private fun Bitmap.boxFit(width: Int, height: Int, fitInside: Boolean): Bitmap {
-    val scaledWidth = width.toFloat() / this.width
-    val scaledHeight = height.toFloat() / this.height
-    val scaleRatio = when (fitInside) {
-        true -> scaledHeight.coerceAtMost(scaledWidth)
-        else -> scaledHeight.coerceAtLeast(scaledWidth)
-    }
+    return try {
+        val scaledWidth = width.toFloat() / this.width
+        val scaledHeight = height.toFloat() / this.height
+        val scaleRatio = when (fitInside) {
+            true -> scaledHeight.coerceAtMost(scaledWidth)
+            else -> scaledHeight.coerceAtLeast(scaledWidth)
+        }
 
-    val matrix = Matrix().also { e -> e.postScale(scaleRatio, scaleRatio) }
-    return Bitmap.createBitmap(
-        this,
-        0,
-        0,
-        this.width,
-        this.height,
-        matrix,
-        true
-    )
+        val matrix = Matrix().apply { postScale(scaleRatio, scaleRatio) }
+        return Bitmap.createBitmap(
+            this,
+            0,
+            0,
+            this.width,
+            this.height,
+            matrix,
+            true
+        )
+    } catch (e: Exception) {
+        Log.e("BitmapUtils", "boxFit: $e")
+        this
+    }
 }
+
